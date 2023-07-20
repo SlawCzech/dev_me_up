763501da6729e5594f52cbf42ff6f6027301e213
SlawCzech <slaw.czech@gmail.com>
2023-07-20 22:12:10 +0200

    changed user model and migrations -> new user not active

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False,
                                                  help_text='Designates whether the user can log into this admin site.',
                                                  verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True,
+                ('is_active', models.BooleanField(default=False,
                                                   help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
                                                   verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
--- backend/users/models.py
+++ backend/users/models.py
@@ -7,11 +7,18 @@ from django.utils.translation import gettext_lazy as _
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
     username = models.CharField(_("username"),
-                                max_length=100,
+                                max_length=50,
                                 unique=False,
                                 help_text=_(
-                                    "Required. 100 characters or fewer."
+                                    "Required. 50 characters or fewer."
                                 ), )
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ), )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
